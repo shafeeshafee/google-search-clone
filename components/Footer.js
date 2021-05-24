3f51b1446e9c58391fda7bfa571c3b7abcca126a
Shafee <shafeelinks@gmail.com>
2021-05-24 01:59:14 -0400

    margin fix for footer items

--- components/Footer.js
+++ components/Footer.js
@@ -11,12 +11,12 @@ function Footer() {
 					<GlobeIcon className="h-5 mr-1 text-green-700" />
 					Carbon neutral since 2007
 				</div>
-				<div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
+				<div className="flex justify-center mx-6 space-x-8 whitespace-nowrap md:justify-self-start">
 					<p>Advertising</p>
 					<p>Business</p>
 					<p>How Search Works</p>
 				</div>
-				<div className="flex justify-center space-x-8 whitespace-nowrap md:ml-auto">
+				<div className="flex justify-center mx-6 space-x-10 whitespace-nowrap md:ml-auto">
 					<p>Privacy</p>
 					<p>Terms</p>
 					<p>Settings</p>
